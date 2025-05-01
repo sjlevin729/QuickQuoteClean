cf742a82c3394a12c497581993b2a0fa65a7dc3b
sjlevin729 <seamus.levin.uk@gmail.com>
2025-05-01 18:11:39 +0100

    Explicitly set filename for HTML output in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      filename: 'index.html'
     })
   ],
   devServer: {
